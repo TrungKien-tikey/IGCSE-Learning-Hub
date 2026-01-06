fc5ee039bbaeca9c7c96d820191384494500d884
Nguyen Truong An <garanhutmau256@gmail.com>
2026-01-06 12:23:21 +0700

    ILH-48 FE - Tích hợp API Đăng nhập & Xử lý lưu Token

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // Chuyển hướng các request /api sang Backend 8080
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
