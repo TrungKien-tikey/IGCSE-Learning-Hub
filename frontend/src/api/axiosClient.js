794971114c3ff4156357f2a74e873e6e21c2dc07
HongTrungKien <hongtrungkien.gr2022@gmail.com>
2026-02-02 13:07:38 +0700

    Deloy web

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. [SỬA LẠI] Dùng đường dẫn tương đối để đi qua Proxy/Gateway
 const axiosClient = axios.create({
-  baseURL: '/api/v1', 
+  baseURL: import.meta.env.VITE_API_URL + '/api/v1',
   headers: {
     'Content-Type': 'application/json',
     "ngrok-skip-browser-warning": "69420",
